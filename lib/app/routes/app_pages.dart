02b16fa1f02a14103d55f6744d63c72d226d97dc
Larissa Ferreira <larissa.ferreira@kobe.io>
2022-06-06 10:34:35 -0300

    feat: add new route

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,6 +1,7 @@
 import 'package:get/route_manager.dart';
 import 'package:tmdbmovies/app/bindings/login_binding.dart';
 import 'package:tmdbmovies/app/routes/app_routes.dart';
+import 'package:tmdbmovies/app/ui/pages/details_page.dart';
 import 'package:tmdbmovies/app/ui/pages/home_page.dart';
 import 'package:tmdbmovies/app/ui/pages/initial_page.dart';
 import 'package:tmdbmovies/app/ui/pages/login_page.dart';
@@ -24,6 +25,10 @@ class AppPages {
     GetPage(
       name: Routes.HOME,
       page: () => HomePage(),
+    ),
+    GetPage(
+      name: Routes.DETAILS,
+      page: () => DetailPage(),
     ),
   ];
 }
